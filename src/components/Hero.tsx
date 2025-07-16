c6f43c799d1e19483f4358ef8953f7d610f9f293
Polkan37 <zaq2587413699@gmail.com>
2025-07-16 23:32:22 +0300

    mobile update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image';
+import BasicUserInfo from './BasicUserInfo';
 
 export default function Hero() {
     return(
       <div className="content-container hero-2">
         <div className="small-corners"><div></div><div></div><div></div><div></div></div>
+        <BasicUserInfo />
         <Image 
             src="/warrior.webp" 
             alt="warrior character"
